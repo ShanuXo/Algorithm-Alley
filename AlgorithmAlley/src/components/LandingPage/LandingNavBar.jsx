1a68b06bf785227556d361f0a8730147fb5008bb
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2024-04-11 23:57:26 +0530

    Landing Page v-1.0.0

---
+++ AlgorithmAlley/src/components/LandingPage/LandingNavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingNavBar = () => {
+  return (
+    <div>LandingNavBar</div>
+  )
+}
+
+export default LandingNavBar
---
+++ AlgorithmAlley/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,21 @@
+const LandingPage = () => {
+  return (
+    <>
+    <div>
+    {/* NavBar */}
+    </div>
+    {/* HeroSection */}
+    <div> 
+        <div>Leftt</div>
+        <div>Right</div>
+    </div>
+    <div>
+        {/* Footer */}
+    </div>
+
+
+    </>
+  )
+}
+
+export default LandingPage
