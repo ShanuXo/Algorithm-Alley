ce8b7c08afa7b23f2664770dc953a8b00a8c3e2b
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2024-04-13 01:28:43 +0530

    basic layout wireframe Landing page

--- AlgorithmAlley/src/components/LandingPage/LandingPage.jsx
+++ AlgorithmAlley/src/components/LandingPage/LandingPage.jsx
@@ -1,21 +1,61 @@
+import LandingNavBar from "./LandingNavBar";
+import Grid from "@mui/material/Grid";
+
 const LandingPage = () => {
   return (
     <>
-    <div>
-    {/* NavBar */}
-    </div>
-    {/* HeroSection */}
-    <div> 
-        <div>Leftt</div>
-        <div>Right</div>
-    </div>
-    <div>
-        {/* Footer */}
-    </div>
-
-
+      <div style={{height:"8vh"}}>
+        <LandingNavBar />
+      </div>
+      {/* HeroSection */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid container spacing={2} columns={6}>
+          <Grid item xs={6}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+                backgroundColor:"lightblue",
+                border:"dashed"
+              }}
+            >
+              Left Section
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} columns={6}>
+          <Grid item xs={6}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+                backgroundColor:"lightblue",
+                border:"dashed",
+                
+              }}
+            >
+              Right Section
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div style={{display:"flex",justifyContent:"center",alignItems:"center",backgroundColor: "lightgray",height:"9vh"}}>
+        <div>
+          Footer
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
